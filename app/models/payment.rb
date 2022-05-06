9018716628d076547ce00b7e4c71f5f4a356b68d
koheitakahashi <426koohei@gmail.com>
2022-05-06 11:02:55 +0900

    支払のCRUD機能を追加

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,2 +1,9 @@
 class Payment < ApplicationRecord
+  validates :amount, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :paid_at, presence: true
+  validates :kind, presence: true
+  validates :name, presence: true
+  validates :note, presence: true
+
+  enum :kind, %i[half individual]
 end
