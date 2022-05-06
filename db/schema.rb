47a6a2bc55d9a21e38c06bdebf21b41c85f5eee9
koheitakahashi <426koohei@gmail.com>
2022-05-06 10:33:57 +0900

    bin/rails g scaffold payment amount:integer paid_at:datetime kind:integer name:string note:text

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_06_013150) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "payments", force: :cascade do |t|
+    t.integer "amount", null: false
+    t.datetime "paid_at", null: false
+    t.integer "kind", null: false
+    t.string "name", default: "", null: false
+    t.text "note", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
