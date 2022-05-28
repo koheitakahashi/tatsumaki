b6211a4ecc8757e032d2eb0eb1c9c219cb6dd74d
koheitakahashi <426koohei@gmail.com>
2022-05-28 15:15:29 +0900

    User Model を追加

--- db/schema.rb
+++ db/schema.rb
@@ -25,8 +25,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_28_023217) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
+    t.datetime "confirmed_at"
     t.string "email", null: false
+    t.string "name", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
