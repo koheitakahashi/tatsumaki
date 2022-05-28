a8ca144881719c7970e6e97bbbcf07d97f6185ca
koheitakahashi <426koohei@gmail.com>
2022-05-28 15:15:33 +0900

    ユーザーログインページを追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      # TODO: 翻訳ファイルにする
+      redirect_to root_path, notice: "Please check your email for confirmation instructions."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :name, :password, :password_confirmation)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
 class User < ApplicationRecord
+  CONFIRMATION_TOKEN_EXPIRATION = 10.minutes
+
+  has_secure_password
+
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP },
+                    presence: true,
+                    uniqueness: true
+  validates :name, presence: true
+
+  def confirm!
+    update_columns(confirmed_at: Time.current)
+  end
+
+  def confirmed?
+    confirmed_at.present?
+  end
+
+  def generate_confirmation_token
+    signed_id expires_in: CONFIRMATION_TOKEN_EXPIRATION, purpose: :confirm_email
+  end
 end
