38b6901c211b1e4785a1d3e325158566e10044f0
koheitakahashi <426koohei@gmail.com>
2022-06-14 07:48:25 +0900

    支出一覧画面の種別のセレクトボックスの体裁を整えた

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -5,4 +5,13 @@ class Payment < ApplicationRecord
   validates :name, presence: true
 
   enum :kind, %i[half individual]
+
+  def self.kinds_18n
+    kinds_hash = {}
+    kinds.keys.each do |key|
+      kinds_hash[I18n.t("activerecord.enums.payment.kind.#{key}")] = key
+    end
+
+    kinds_hash
+  end
 end
