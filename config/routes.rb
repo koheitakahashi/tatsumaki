7e33e5a51c45e3b2b9b52b1b5761544e9bd17dcc
koheitakahashi <426koohei@gmail.com>
2022-06-14 11:21:58 +0900

    tmp routes を更新

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root "monthly_expenses#show"
   resources :payments, only: %i[new create edit update destroy]
   get "monthly_expenses/:year/:month", to: "monthly_expenses#show", as: "monthly_expenses"
+
+  post "sign_up", to: "users#create"
+  get "sign_up", to: "users#new"
+  resources :users, only: %i[edit update destroy]
+  resources :confirmations, only: [:create, :edit, :new], param: :confirmation_token
 end
