64072ef4af2f0b0fcbfc8c09d66d66854bba0794
koheitakahashi <426koohei@gmail.com>
2022-06-14 09:19:55 +0900

    Model Spec を追加

---
+++ spec/models/payment_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Payment, type: :model do
+  describe ".kinds_18n" do
+    it "翻訳されたkey と 翻訳されていない value の Hash が返る" do
+      expected = { "個別" => "individual", "折半" => "half" }
+      expect(Payment.kinds_18n).to eq expected
+    end
+  end
+end
